48615a0f3be4cd7cb2b38a292d4f5ed6b9cbc42b
Sixte de Maupeou D'Ableiges <sixtedemaupeou@gmail.com>
2016-11-19 17:47:21 +0000

    Add background and controls

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float movespeed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Touch touch in Input.touches)
+		{
+			rb.velocity = new Vector2(movespeed, rb.velocity.y);
+		}
+	}
+}
